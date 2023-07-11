2ec0e9d1230aecd3026d548d6dd432de6cee5a61
Damian <dsliwa9913@gmail.com>
2023-07-11 21:58:21 +0200

    Transformed application to MVVM.

---
+++ STR_ART_V/Model/ImageUtilities.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace STR_ART_V.Model
+{
+    public static class ImageUtilities
+    {
+        public static BitmapImage CreateBitmap(string imagePath)
+        {
+            var bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(imagePath);
+            bitmap.EndInit();
+
+            return bitmap;
+        }
+
+        public static BitmapSource ResizeImage(BitmapImage sourceImage, int newWidth, int newHeight)
+        {
+            // Utworzenie nowego obrazu o podanych rozmiarach
+            BitmapImage resizedImage = new BitmapImage();
+            resizedImage.BeginInit();
+            resizedImage.DecodePixelWidth = newWidth;
+            resizedImage.DecodePixelHeight = newHeight;
+            resizedImage.CacheOption = BitmapCacheOption.OnLoad;
+            resizedImage.UriSource = sourceImage.UriSource;
+            resizedImage.EndInit();
+            resizedImage.Freeze(); // Zamrożenie obrazu, aby można go było używać na wątku interfejsu użytkownika
+
+            return resizedImage;
+        }
+
+        public static BitmapSource DetectEdges(BitmapSource sourceImage)
+        {
+            // Konwersja na obraz w skali szarości
+            FormatConvertedBitmap grayScaleBitmap = new FormatConvertedBitmap(sourceImage, PixelFormats.Gray8, null, 0);
+
+            // Wykrywanie krawędzi (konturów) z użyciem operatora Sobela
+            BitmapSource edgeImage = SobelEdgeDetector.DetectEdges(grayScaleBitmap);
+
+            return edgeImage;
+        }
+
+        public static BitmapSource ApplyRedPixels(BitmapSource image, int redPixelCount)
+        {
+            int width = image.PixelWidth;
+            int height = image.PixelHeight;
+
+            // Pobierz współrzędne białych pikseli
+            List<(int x, int y)> whitePixelCoordinates = GetWhitePixelCoordinates(image);
+
+            if (redPixelCount > whitePixelCoordinates.Count)
+            {
+                redPixelCount = whitePixelCoordinates.Count;
+
+                throw new ArgumentException("Too many red pixels.");
+            }
+
+            // Ustawienie ziarna generatora liczb losowych
+            Random random = new Random();
+
+            // Oblicz odstęp między pikselami w celu równomiernego rozmieszczenia
+            double interval = (double)whitePixelCoordinates.Count / redPixelCount;
+
+            // Tworzenie docelowego obrazu z modyfikacjami
+            RenderTargetBitmap resultImage = new RenderTargetBitmap(width, height, 96, 96, PixelFormats.Pbgra32);
+            resultImage.Clear();
+
+            // Tworzenie kontekstu renderowania
+            DrawingVisual drawingVisual = new DrawingVisual();
+            using (DrawingContext drawingContext = drawingVisual.RenderOpen())
+            {
+                // Rysowanie oryginalnego obrazu
+                drawingContext.DrawImage(image, new Rect(0, 0, width, height));
+
+                // Iteracja po wybranych pikselach białych i nanieś czerwone piksele
+                double position = 0;
+                int count = 0;
+
+                while (count < redPixelCount)
+                {
+                    // Oblicz indeks piksela białego na podstawie pozycji
+                    int index = (int)(position * interval);
+                    (int x, int y) = whitePixelCoordinates[index];
+
+                    // Nanieś czerwony piksel
+                    drawingContext.DrawRectangle(Brushes.Red, null, new Rect(x, y, 1, 1));
+                    count++;
+
+                    position += 1;
+
+                    // Jeśli przekroczono liczbę czerwonych pikseli lub nie ma już dostępnych białych pikseli, przerwij pętlę
+                    if (count >= redPixelCount || position >= whitePixelCoordinates.Count)
+                        break;
+                }
+            }
+
+            // Zakończenie renderowania i zwrócenie docelowego obrazu
+            resultImage.Render(drawingVisual);
+            return resultImage;
+        }
+
+        private static List<(int x, int y)> GetWhitePixelCoordinates(BitmapSource image)
+        {
+            int width = image.PixelWidth;
+            int height = image.PixelHeight;
+            int stride = width * ((image.Format.BitsPerPixel + 7) / 8);
+            byte[] pixels = new byte[height * stride];
+            image.CopyPixels(pixels, stride, 0);
+
+            List<(int x, int y)> whitePixelCoordinates = new List<(int x, int y)>();
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    if (pixels[y * stride + x] == 255)
+                        whitePixelCoordinates.Add((x, y));
+                }
+            }
+
+            return whitePixelCoordinates;
+        }
+    }
+}
--- STR_ART_V/ViewModel/MainWindowViewModel.cs
+++ STR_ART_V/ViewModel/MainWindowViewModel.cs
@@ -1,17 +1,94 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Win32;
+using STR_ART_V.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace STR_ART_V.ViewModel
 {
-    public class MainWindowViewModel : ObservableObject
+    public partial class MainWindowViewModel : ObservableObject
     {
         public MainWindowViewModel()
         {
-            
+            LoadImageCommand = new RelayCommand(LoadImage, CanLoadImage); 
+        }
+
+        public IRelayCommand LoadImageCommand { get; }
+
+        [ObservableProperty]
+        private string _redPixelCount = string.Empty;
+
+        [ObservableProperty]
+        private ImageSource? _processedImage;
+
+        partial void OnRedPixelCountChanged(string value)
+        {
+            LoadImageCommand.NotifyCanExecuteChanged();
+        }
+
+        private void LoadImage()
+        {
+            var imagePath = FileDialogUtilities.OpenImageFile();
+
+            if(imagePath is null)
+            {
+                return;
+            }
+
+            var bitmap = ImageUtilities.CreateBitmap(imagePath);
+
+            //Powiększenie obrazu do rozmiaru 3000x3000
+            var resizedImage = ImageUtilities.ResizeImage(bitmap, 3000, 3000);
+
+            // Wykrycie krawędzi (konturów)
+            var edgeImage = ImageUtilities.DetectEdges(resizedImage);
+
+            //Wywołanie metody ApplyRedPixels dla przetworzonego obrazu
+            int? redPixelCount = SystemUtilities.ParseStringToInt(RedPixelCount);
+
+            if(redPixelCount is null)
+            {
+                return;
+            }
+
+            BitmapSource? processedImage = null;
+            try
+            {
+                processedImage = ImageUtilities.ApplyRedPixels(edgeImage, redPixelCount.Value);
+            }
+            catch(ArgumentException)
+            {
+                MessageBox.Show("Red pixel count exceeds the number of white pixels. Adjusted to the maximum possible value.");
+            }
+
+            if(processedImage is null)
+            {
+                return;
+            }
+
+            //Wyświetlenie przetworzonego obrazu
+            ProcessedImage = processedImage;
+
+            //Zapisanie obrazu do pliku
+            SystemUtilities.SaveImage(imagePath, processedImage);
+        }
+
+        private bool CanLoadImage()
+        {
+            if (SystemUtilities.ParseStringToInt(RedPixelCount) is null)
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
